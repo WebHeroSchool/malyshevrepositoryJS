d45534a15da8fb9f43a4cfb931eb6e89ebfabb25
Maxim Malyshev <malyshev21@gmail.com>
2020-06-21 17:03:31 +0300

    final work

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert ('Hello World!')
+const levels = [
+  document.getElementById('simple'),
+  document.getElementById('middle'),
+  document.getElementById('hard')
+];
+
+levels.forEach(setLevel => {
+	setLevel.onclick = function() {
+		levels.forEach(setLevels => setLevels.classList.remove('level-selector'));
+		this.classList.add('level-selector');
+	};
+});
+
+
+
+
+
+
